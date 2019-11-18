cdddc550238f4330ac7ee0f2ceae260bb4fe8a50
Tom Bosmans <tom.bosmans@be.ibm.com>
2019-11-18 16:19:55 +0100

    List and Show role and modifications to isamadmin.py

--- start_config/library/isamadmin.py
+++ start_config/library/isamadmin.py
@@ -15,20 +15,25 @@ from ibmsecurity.user.applianceuser import ApplianceUser
 from ibmsecurity.user.isamuser import ISAMUser
 
 logger = logging.getLogger(sys.argv[0])
-
+try:
+    basestring
+except NameError:
+    basestring = (str, bytes)
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            log=dict(default='INFO', choices=['DEBUG', 'INFO', 'ERROR', 'CRITICAL']),
+            log=dict(required=False, default='INFO', choices=['DEBUG', 'INFO', 'ERROR', 'CRITICAL']),
             appliance=dict(required=True),
             lmi_port=dict(required=False, default=443, type='int'),
             username=dict(required=False),
             password=dict(required=True, no_log=True),
             isamuser=dict(required=False),
-            isampwd=dict(required=True, no_log=True),
+            isampwd=dict(required=False, no_log=True),
+            action=dict(required=False, default=''),
+            isamapi=dict(required=False, type='dict'),
             isamdomain=dict(required=False, default='Default'),
-            commands=dict(required=True, type='list'),
+            commands=dict(required=False, type='list'),
             adminProxyProtocol=dict(required=False, default='https', choices=['http','https']),
             adminProxyHostname=dict(required=False),
             adminProxyPort=dict(required=False, default=443, type='int'),
@@ -41,7 +46,14 @@ def main():
     module.debug('Started isamadmin module')
 
     # Process all Arguments
+    #  The specific arguments are
+    #   isamuser
+    #   isampwd
+    #
+    #  For backwards compatibility, isamdomain and commands are also still in here.
+    #
     logLevel = module.params['log']
+    action = module.params['action']
     appliance = module.params['appliance']
     lmi_port = module.params['lmi_port']
     username = module.params['username']
@@ -105,43 +117,78 @@ def main():
                                               adminProxyApplianceShortName=adminProxyApplianceShortName)
 
     if isamuser == '' or isamuser is None:
-        iu = ISAMUser(password=isampwd)
+        if isampwd == '' or isampwd is None:
+            iu = ISAMUser(password=isampwd)
+        else:
+            iu = ''
     else:
         iu = ISAMUser(username=isamuser, password=isampwd)
 
-    try:
-        import ibmsecurity.isam.web.runtime.pdadmin
+    # Default the action to pdadmin execute for backwards compatibility with
+    # the roles execute_pdadmin, web/execute_pdadmin and execute_isamcfg
+    if action == '' or action is None:
+        action = 'ibmsecurity.isam.web.runtime.pdadmin.execute'
+        options = 'isamAppliance=isam_server, isamUser=iu, admin_domain=isamdomain, commands=commands'
+    else:
+        # Create options string to pass to action method
+        # default to isamAppliance=isam_server, isamUser=iu, admin_domain=isamdomain, commands=commands
+        options = 'isamAppliance=isam_server'
+        if iu is not None or iu != '':
+            options = options + ', isamUser=iu'
+        if isinstance(module.params['isamapi'], dict):
+            for key, value in module.params['isamapi'].items():
+                if isinstance(value, basestring):
+                    options = options + ', ' + key + '="' + value + '"'
+                else:
+                    options = options + ', ' + key + '=' + str(value)
 
-        startd = datetime.datetime.now()
+    module.debug('Option to be passed to action: ' + options)
 
-        ret_obj = ibmsecurity.isam.web.runtime.pdadmin.execute(isamAppliance=isam_server, isamUser=iu,
-                                                               admin_domain=isamdomain, commands=commands)
+    # Dynamically process the action to be invoked
+    # Simple check to restrict calls to just "isam" ones for safety
+    if action.startswith('ibmsecurity.isam.'):
+        try:
+            module_name, method_name = action.rsplit('.', 1)
+            module.debug('Action method to be imported from module: ' + module_name)
+            module.debug('Action method name is: ' + method_name)
+            mod = importlib.import_module(module_name)
+            func_ptr = getattr(mod, method_name)  # Convert action to actual function pointer
+            func_call = 'func_ptr(' + options + ')'
 
-        endd = datetime.datetime.now()
-        delta = endd - startd
+            startd = datetime.datetime.now()
+            # Execute requested 'action'
+            #   ret_obj = ibmsecurity.isam.web.runtime.pdadmin.execute(isamAppliance=isam_server, isamUser=iu,
+            #                                                                admin_domain=isamdomain, commands=commands)
+            ret_obj = eval(func_call)
 
-        ret_obj['stdout'] = strlog.getvalue()
-        ret_obj['stdout_lines'] = strlog.getvalue().split()
-        ret_obj['start'] = str(startd)
-        ret_obj['end'] = str(endd)
-        ret_obj['delta'] = str(delta)
-        ret_obj[
-            'cmd'] = "ibmsecurity.isam.config_fed_dir.runtime.pdadmin.execute(isamAppliance=isam_server, isamUser=iu, commands=" + str(
-            commands) + ")"
-        ret_obj['ansible_facts'] = isam_server.facts
+            endd = datetime.datetime.now()
+            delta = endd - startd
 
-        module.exit_json(**ret_obj)
+            ret_obj['stdout'] = strlog.getvalue()
+            ret_obj['stdout_lines'] = strlog.getvalue().split()
+            ret_obj['start'] = str(startd)
+            ret_obj['end'] = str(endd)
+            ret_obj['delta'] = str(delta)
+            ret_obj['cmd'] = action + "(" + options + ")"
+            ret_obj['ansible_facts'] = isam_server.facts
 
-    except ImportError:
-        module.fail_json(name='pdadmin', msg='Error> Unable to import pdadmin module!', log=strlog.getvalue())
-    except AttributeError:
-        module.fail_json(name='pdadmin', msg='Error> Error finding execute function of pdadmin module',
+            module.exit_json(**ret_obj)
+
+        except ImportError:
+            module.fail_json(name=action, msg='Error> action belongs to a module that is not found!',
+                             log=strlog.getvalue())
+        except AttributeError:
+            module.fail_json(name=action, msg='Error> invalid action was specified, method not found in module!',
+                             log=strlog.getvalue())
+        except TypeError:
+            module.fail_json(name=action,
+                             msg='Error> action does not have the right set of arguments or there is a code bug! Options: ' + options,
+                             log=strlog.getvalue())
+        except IBMError as e:
+            module.fail_json(name=action, msg=str(e), log=strlog.getvalue())
+    else:
+        module.fail_json(name=action, msg='Error> invalid action specified, needs to be isam!',
                          log=strlog.getvalue())
-    except TypeError:
-        module.fail_json(name='pdadmin', msg='Error> pdadmin has wrong set of arguments or there is a bug in code!',
-                         log=strlog.getvalue())
-    except IBMError as e:
-        module.fail_json(name='pdadmin', msg=str(e), log=strlog.getvalue())
 
 
 if __name__ == '__main__':
